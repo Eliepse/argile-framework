d75f981b92fc4036d854b5d80fc1a14f22dc6f2a
Élie Meignan <contact@eliepse.fr>
2021-04-11 13:30:32 +0200

    feat: session management

---
+++ src/Http/Middleware/SessionMiddleware.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Eliepse\Argile\Http\Middleware;
+
+use Eliepse\Argile\Config\ConfigRepository;
+use Middlewares\PhpSession;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+final class SessionMiddleware implements \Psr\Http\Server\MiddlewareInterface
+{
+	private PhpSession $phpSession;
+
+
+	public function __construct(ConfigRepository $configs)
+	{
+		$this->phpSession = new PhpSession();
+
+		$this->phpSession->name($configs->get("app.session.name", "argile_session"));
+
+		$this->phpSession->options([
+			'use_strict_mode' => true,
+			'cookie_httponly' => true,
+			'use_only_cookies' => true,
+			'use_trans_sid' => true,
+			'sid_length' => 64,
+			'sid_bits_per_character' => 6,
+			'cookie_lifetime' => $configs->get("app.session.lifetime", 3_600 * 24 * 14),
+		]);
+	}
+
+
+	public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+	{
+		return $this->phpSession->process($request, $handler);
+	}
+}
