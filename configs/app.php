15e53300b957a67ae0da47f0895b64c6c4677c38
Élie Meignan <contact@eliepse.fr>
2024-03-31 11:22:22 +0200

    Fix and add secure config

--- configs/app.php
+++ configs/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use Eliepse\Argile\Core\Application;
 use Eliepse\Argile\Providers\CacheProvider;
 use Eliepse\Argile\Providers\FilesystemProvider;
 use Eliepse\Argile\Providers\ViewProvider;
@@ -8,6 +9,7 @@ return [
 
 	"session" => [
 		"name" => "argile_session",
+        "secure" => Application::getInstance()->isProduction(),
 		"lifetime" => 3_600 * 24 * 14, // 14 days
 	],
 
--- src/Http/Middleware/SessionMiddleware.php
+++ src/Http/Middleware/SessionMiddleware.php
@@ -3,6 +3,7 @@
 namespace Eliepse\Argile\Http\Middleware;
 
 use Eliepse\Argile\Config\ConfigRepository;
+use Eliepse\Argile\Core\Application;
 use Middlewares\PhpSession;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -13,7 +14,7 @@ final class SessionMiddleware implements \Psr\Http\Server\MiddlewareInterface
 	private PhpSession $phpSession;
 
 
-	public function __construct(ConfigRepository $configs)
+	public function __construct(Application $app, ConfigRepository $configs)
 	{
 		$this->phpSession = new PhpSession();
 
@@ -24,9 +25,10 @@ final class SessionMiddleware implements \Psr\Http\Server\MiddlewareInterface
 			'cookie_httponly' => true,
 			'sid_length' => 64,
 			'sid_bits_per_character' => 6,
-            "use_cookie" => false,
+            "use_cookies" => true,
             "use_only_cookie" => true,
             "use_trans_id" => false,
+            "cookie_secure" => $configs->get("secure", $app->isProduction()),
 			'cookie_lifetime' => $configs->get("app.session.lifetime", 3_600 * 24 * 14),
 		]);
 	}
